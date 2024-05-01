27c24151892e1245a5707db3baa8bfb5eebb43a8
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-05-01 12:32:33 +0100

    Build out {"hello":"world"} reply to all endpoints.

---
+++ src/app/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def hello_world(**kwargs):
+    if len(kwargs) < 1:
+        return "Hello, World!"
+    else:
+        str = """"""
+        for key, value in kwargs.items():
+            str += f"the {key} is {value}\n"
+        return str
+
+app.add_url_rule("/api/encounters/", methods=["GET", "POST"], view_func=hello_world)
+app.add_url_rule("/api/encounters/<id>/", methods=["GET", "PATCH"], view_func=hello_world)
+app.add_url_rule("/api/plans/", methods=["GET", "POST"], view_func=hello_world)
+app.add_url_rule("/api/plans/<id>/", methods=["GET", "PATCH"], view_func=hello_world)
+
+if __name__ == '__main__':
+    app.run(debug=True)
