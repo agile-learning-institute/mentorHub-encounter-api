e6d23da26446c1d347c81289c623701b8d9537dd
Mike Storey <mike@agile-learning.institute>
2024-12-10 19:31:46 -0500

    plan services tested

--- src/services/plan_services.py
+++ src/services/plan_services.py
@@ -12,72 +12,30 @@ class PlanService:
     # TODO: Review all 
 
     @staticmethod 
-    def _check_user_access(data, token):
+    def _check_user_access(token):
         """Role Based Access Control logic"""
         
         # Staff can edit all plans
         if "Staff" in token["roles"]: return
         
-        # Members can access their own plans
-        if "Member" in token["roles"] and data["personId"] == token["user_id"]: return
-        
-        # Mentors can access their apprentices plans
-        if "Mentor" in token["roles"]:
-            if data["mentorId"] == token["user_id"]:
-                return
+        # Mentors can edit all plans
+        if "Mentor" in token["roles"]: return
         
         # User has No Access! Log a warning and raise an exception
-        logger.warning(f"Access Denied: {data}, {token}")
+        logger.warning(f"Access Denied: {token}")
         raise Exception("Access Denied")
-      
-    @staticmethod
-    def _mongo_encode(document):
-        """Encode ObjectId and datetime values for MongoDB"""
-        id_properties = ["personId", "mentorId", "planId", "byUser"]
-        date_properties = ["date", "atTime"]
-        
-        def encode_value(key, value):
-            """Encode identified values"""
-            if key in id_properties:
-                if isinstance(value, str):
-                    return ObjectId(value)
-                if isinstance(value, list):
-                    return [ObjectId(item) if isinstance(item, str) else item for item in value]
-            if key in date_properties:
-                if isinstance(value, str):
-                    return datetime.fromisoformat(value)
-                if isinstance(value, list):
-                    return [datetime.fromisoformat(item) if isinstance(item, str) else item for item in value]
-            return value
-
-        # Traverse the document and encode relevant properties
-        for key, value in document.items():
-            if isinstance(value, dict):
-                PlanService._mongo_encode(value)  
-            elif isinstance(value, list):
-                if all(isinstance(item, dict) for item in value):
-                    document[key] = [PlanService._mongo_encode(item) for item in value]
-                else:
-                    document[key] = [encode_value(key, item) for item in value]
-            else:
-                document[key] = encode_value(key, value)  
-
-        return document
-    
+         
     @staticmethod
     def create_plan(data, token, breadcrumb):
         """Get a plan if it exits, if not create a new one and return that"""
-        collection_name = config.get_plans_collection_name()
-        PlanService._check_user_access(data, token)
+        PlanService._check_user_access(token)
         
         # Add breadcrumb and Active status
         data["lastSaved"] = breadcrumb
         data["status"] = "Active"
         
-        # Encode Mongo ObjectID and Dates
-        PlanService._mongo_encode(data)
-        
         # Add the document and fetch the updated document
+        collection_name = config.PLANS_COLLECTION_NAME
         new_plan_id = mongoIO.create_document(collection_name, data)
         plan = mongoIO.get_document(collection_name, new_plan_id)
         return plan
@@ -85,25 +43,21 @@ class PlanService:
     @staticmethod
     def get_plan(plan_id, token):
         """Get a plan if the user has access"""
-        collection_name = config.get_plans_collection_name()
+        PlanService._check_user_access(token)
+        collection_name = config.PLANS_COLLECTION_NAME
         plan = mongoIO.get_document(collection_name, plan_id)
-        PlanService._check_user_access(plan, token)
         return plan
 
     @staticmethod
     def update_plan(plan_id, patch_data, token, breadcrumb):
         """Update the specified plan"""
-        collection_name = config.get_plans_collection_name()
-        plan = mongoIO.get_document(collection_name, plan_id)
-        PlanService._check_user_access(plan, token)
+        PlanService._check_user_access(token)
 
-        # Add breadcrumb and Active status
+        # Add breadcrumb 
         patch_data["lastSaved"] = breadcrumb
         
-        # Encode Mongo ObjectID and Dates
-        PlanService._mongo_encode(patch_data)
-        
         # Update the document - the updated document is returned
+        collection_name = config.PLANS_COLLECTION_NAME
         plan = mongoIO.update_document(collection_name, plan_id, patch_data)
         return plan
 
--- src/services/test_plan_services.py
+++ src/services/test_plan_services.py
@@ -2,7 +2,7 @@ import unittest
 from unittest.mock import patch, MagicMock
 from bson import ObjectId
 from datetime import datetime, timezone
-from src.services.plan_services import planService
+from src.services.plan_services import PlanService
 
 
 class TestEncounterService(unittest.TestCase):
@@ -13,96 +13,75 @@ class TestEncounterService(unittest.TestCase):
         mock_create_document.return_value = "mock_plan_id"
         mock_get_document.return_value = {"id": ObjectId("000000000000000000000001"), "status": "Active"}
 
-        data = {
-            "personId": str(ObjectId()),
-            "mentorId": str(ObjectId()), 
-            "planId": str(ObjectId())
-        }
+        data = {"name": "Foo"}
         token = {"user_id": str(ObjectId()), "roles": ["Staff"]}
-        breadcrumb = {
-            "atTime": datetime.now(timezone.utc),
-            "byUser": ObjectId(),
-            "fromIp": "127.0.0.1",
-            "correlationId": "test-correlation-id"
-        }
+        breadcrumb = {"atTime": datetime.now(timezone.utc),"byUser": ObjectId(),"fromIp": "127.0.0.1","correlationId": "test-correlation-id"}
 
-        result = planService.create_plan(data, token, breadcrumb)
+        result = PlanService.create_plan(data, token, breadcrumb)
         self.assertEqual(result, {"id": ObjectId("000000000000000000000001"), "status": "Active"})
 
         # Assertions for MongoIO interactions
         mock_create_document.assert_called_once_with(
             "plans",
             {
-                "personId": ObjectId(data["personId"]),
-                "mentorId": ObjectId(data["mentorId"]),
-                "planId": ObjectId(data["planId"]),
-                "status": "Active",
-                "lastSaved": breadcrumb
+                "name": "Foo",
+                "lastSaved": breadcrumb,
+                "status": "Active"
             }
         )
         mock_get_document.assert_called_once_with("plans", "mock_plan_id")
 
     @patch('src.utils.mongo_io.MongoIO.get_document')
     def test_get_plan(self, mock_get_document):
-        test_document = {"personId": str(ObjectId()), "mentorId": str(ObjectId()), "planId": str(ObjectId())}
-        mock_get_document.side_effect = [test_document]
+        mock_get_document.side_effect = [{"name": "foo"}]
 
         plan_id = "mock_plan_id"
         token = {"user_id": str(ObjectId()), "roles": ["Staff"]}
-        breadcrumb = {
-            "atTime": datetime.now(timezone.utc),
-            "byUser": ObjectId(),
-            "fromIp": "127.0.0.1",
-            "correlationId": "test-correlation-id"
-        }
 
-        result = planService.get_plan(plan_id, token)
-        self.assertEqual(result, test_document)
+        result = PlanService.get_plan(plan_id, token)
+        self.assertEqual(result, {"name": "foo"})
 
         # Assertions for MongoIO interactions
         mock_get_document.assert_called_with("plans", plan_id)
 
-    @patch('src.utils.mongo_io.MongoIO.get_document')
     @patch('src.utils.mongo_io.MongoIO.update_document')
-    def test_update_plan(self, mock_update_document, mock_get_document):
-        test_document = {"personId": ObjectId(), "mentorId": ObjectId(), "plan_id": ObjectId()}
-        mock_get_document.side_effect = [test_document]
+    def test_update_plan(self, mock_update_document):
+        test_document = {"name":"foo"}
         mock_update_document.side_effect = [test_document]
 
         plan_id = "mock_plan_id"
         patch_data = {"status": "Updated"}
         token = {"user_id": str(ObjectId()), "roles": ["Staff"]}
-        breadcrumb = {
-            "atTime": datetime.now(timezone.utc),
-            "byUser": ObjectId(),
-            "fromIp": "127.0.0.1",
-            "correlationId": "test-correlation-id"
-        }
+        breadcrumb = {"atTime": datetime.now(timezone.utc),"byUser": ObjectId(),"fromIp": "127.0.0.1","correlationId": "test-correlation-id"}
 
-        result = planService.update_plan(plan_id, patch_data, token, breadcrumb)
+        result = PlanService.update_plan(plan_id, patch_data, token, breadcrumb)
         self.assertEqual(result, test_document)
 
         # Assertions for MongoIO interactions
-        mock_get_document.assert_called_with("plans", plan_id)
         mock_update_document.assert_called_once_with(
             "plans",
-            plan_id,
+            "mock_plan_id",
             {"status": "Updated", "lastSaved": breadcrumb}
         )
 
     def test_check_user_access_staff(self):
-        data = {"personId": str(ObjectId()), "mentor_id": str(ObjectId())}
-        token = {"user_id": str(ObjectId()), "roles": ["Staff"]}
+        token = {"roles": ["Staff"]}
         try:
-            planService._check_user_access(data, token)
+            PlanService._check_user_access(token)
+        except Exception:
+            self.fail("_check_user_access raised Exception unexpectedly!")
+
+    def test_check_user_access_mentor(self):
+        token = {"roles": ["Mentor"]}
+        try:
+            PlanService._check_user_access(token)
         except Exception:
             self.fail("_check_user_access raised Exception unexpectedly!")
 
     def test_check_user_access_access_denied(self):
-        data = {"personId": str(ObjectId()), "mentor_id": str(ObjectId())}
-        token = {"user_id": str(ObjectId()), "roles": ["Member"]}
+        token = {"roles": ["Member"]}
         with self.assertRaises(Exception) as context:
-            planService._check_user_access(data, token)
+            PlanService._check_user_access(token)
         self.assertEqual(str(context.exception), "Access Denied")
 
 
